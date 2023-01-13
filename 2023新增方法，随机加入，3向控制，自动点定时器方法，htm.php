51c27a0a624f707248132cfc5e9e3b9319c72d05
km3xy <77920834+km3xy@users.noreply.github.com>
2023-01-13 23:21:58 +0800

    Create 2023新增方法，随机加入，3向控制，自动点定时器方法，htm.php

---
+++ 2023新增方法，随机加入，3向控制，自动点定时器方法，htm.php
@@ -0,0 +1,181 @@
+//代码片段
+
+</head>
+
+<body>
+
+
+<div>
+  <div>
+      
+      
+<h1  style="font-size:35px;color
+
+:#FF0000" >
+
+核心方法1#  <br>
+
+多 挑 满 格 列 ，少 选 齐 满 位 
+
+<br>
+
+                                                   
+    
+#核心方法2<br>
+
+
+新增随机加入绝对概率自动点，
+
+
+<br>
+
+
+
+<h1 style="font-size:35px;color
+
+:#FF0000" >新增setInterval定时器请等待8秒____<span id="sp3">___</span></h1>
+
+
+<br>
+
+
+
+新增随机加入绝对概率自动点，
+
+
+
+<br>
+
+
+
+#核心方法3<br>
+
+
+绝对概率写入+input
+
+
+<a href="85qqqqqqqqq123456789zj85q.php" target="_blank"  style="font-size:88px;">点我，返回主页</a>
+
+<br>
+
+
+满位，合并，复选
+
+
+<br>
+
+
+<br>
+
+
+然后复制传值到复选框复选
+
+<br>
+    
+
+<br>
+
+
+
+
+</h1>
+
+
+
+
+<button  style="font-size:60px;color:#FF0000" onclick="function()" id='button1'>前区能自动点击按钮</button>  
+
+<script>
+
+
+window.onload=setTimeout(function (){
+    
+    
+    
+    
+var x="",i=0;
+
+    //随机点击次数
+
+
+var a=Math.random();
+
+var d =Math.random();
+
+var time =Math.random();
+
+
+if (time>d)
+
+{
+
+    alert('按钮被点击啦 *15+5次'); 
+    
+var y=Math.floor(Math.random()*15+5); 
+
+} 
+
+
+else if (time<=d && time<a)
+{
+
+
+
+alert('按钮没被点击 *0+0次 '); 
+   
+ var y=Math.floor(Math.random()*0+0);
+
+
+}
+
+
+else{
+
+    alert('按钮被点击啦 *20+5次 '); 
+   
+ var y=Math.floor(Math.random()*20+5); 
+
+}
+
+
+    
+
+    //随机点击次数
+
+    while (i<y){
+
+        x=x + "被点击了 " + i + "<br>";
+
+        i++;
+
+    
+
+document.getElementById('button1').click();
+
+  
+   }
+    
+    
+    
+    
+}, 8000);
+
+
+
+  
+
+
+function  myFunction1(){
+alert('按钮被点击啦'); 
+
+    
+
+
+}
+
+
+</script>
+
+
+//代码片段
+
